453e87d387130026f6f9f500e582c48acaf6bde0
Jens Rydholm <spacejens@gmail.com>
2023-07-29 15:42:49 +0200

    Store wget output file in same output directory as downloaded domain files

--- bbldownload/download-site.sh
+++ bbldownload/download-site.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Error: Must provide hostname of BBL site as argument (e.g. https://tloeg.bbleague.se/)"
+    echo "Error: Must provide hostname of BBL site as argument (e.g. tloeg.bbleague.se)"
     exit 1
 fi
 
 SITE=$1
+mkdir -p bbl-site/$SITE
 
-# TODO Place output file inside directory of downloaded domain files?
 # TODO Ensure that redirects are followed to required depth, see https://stackoverflow.com/questions/20030148/wget-doesnt-download-recursively-after-following-a-redirect and https://unix.stackexchange.com/questions/409804/wget-not-working-for-downloading-all-links-from-a-webpage
 # TODO Unusual characters in URLs, e.g. Rösereds Idrottspack linked as default.asp?p=tm&t=rös ends up as https://tloeg.bbleague.se/default.asp?p=tm&t=r%F6s
 # TODO Converting links to enable navigation, see https://danielmalmer.medium.com/heres-why-wget-s-convert-links-option-isn-t-converting-your-links-cec832ee934c
@@ -20,5 +20,5 @@ wget \
     --convert-links \
     --wait=3 \
     --directory-prefix=bbl-site/ \
-    --output-file=bbl-site/output-wget.log \
+    --output-file=bbl-site/$SITE/output-wget.log \
     $SITE
