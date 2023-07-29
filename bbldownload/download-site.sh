cd20a88291c3777b9650c0c67e4d1ba85ed076b1
Jens Rydholm <spacejens@gmail.com>
2023-07-29 15:31:48 +0200

    Store wget output in log file

--- bbldownload/download-site.sh
+++ bbldownload/download-site.sh
@@ -7,7 +7,7 @@ fi
 
 SITE=$1
 
-# TODO Save log to file using --output-file and/or piping to tee
+# TODO Place output file inside directory of downloaded domain files?
 # TODO Ensure that redirects are followed to required depth, see https://stackoverflow.com/questions/20030148/wget-doesnt-download-recursively-after-following-a-redirect and https://unix.stackexchange.com/questions/409804/wget-not-working-for-downloading-all-links-from-a-webpage
 # TODO Unusual characters in URLs, e.g. Rösereds Idrottspack linked as default.asp?p=tm&t=rös ends up as https://tloeg.bbleague.se/default.asp?p=tm&t=r%F6s
 # TODO Converting links to enable navigation, see https://danielmalmer.medium.com/heres-why-wget-s-convert-links-option-isn-t-converting-your-links-cec832ee934c
@@ -20,4 +20,5 @@ wget \
     --convert-links \
     --wait=3 \
     --directory-prefix=bbl-site/ \
+    --output-file=bbl-site/output-wget.log \
     $SITE
