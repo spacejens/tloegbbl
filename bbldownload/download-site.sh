a2fb9fc1508f9c4386df8f2818d53cb3593a546e
Jens Rydholm <spacejens@gmail.com>
2023-07-30 11:20:57 +0200

    Local encoding argument to wget not needed to fix URLs with åäö

--- bbldownload/download-site.sh
+++ bbldownload/download-site.sh
@@ -9,7 +9,6 @@ SITE=$1
 mkdir -p bbl-site/$SITE
 
 # TODO Ensure that redirects are followed to required depth, see https://stackoverflow.com/questions/20030148/wget-doesnt-download-recursively-after-following-a-redirect and https://unix.stackexchange.com/questions/409804/wget-not-working-for-downloading-all-links-from-a-webpage
-# TODO Unusual characters in URLs, e.g. Rösereds Idrottspack linked as default.asp?p=tm&t=rös ends up as https://tloeg.bbleague.se/default.asp?p=tm&t=r%F6s
 # TODO Converting links to enable navigation, see https://danielmalmer.medium.com/heres-why-wget-s-convert-links-option-isn-t-converting-your-links-cec832ee934c
 # TODO Don't think it's needed, but may want to try --content-disposition, see https://www.reddit.com/r/linuxquestions/comments/zws6ei/wget_issues_with_special_characters_in_deb_url/
 # TODO Wget doesn't follow onclick script setting href, will need to grep for those and loop until no new such links are found. Try: grep "href='?.*'" *
@@ -21,7 +20,6 @@ wget \
     --level=inf \
     --page-requisites \
     --wait=3 \
-    --local-encoding=UTF-8 \
     --remote-encoding=UTF-8 \
     --directory-prefix=bbl-site/ \
     --output-file=bbl-site/$SITE/output-wget.log \
