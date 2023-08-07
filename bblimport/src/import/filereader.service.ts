05abb6f19d18f098dcd88b0e68cc0712a610f536
Jens Rydholm <spacejens@gmail.com>
2023-08-07 19:49:14 +0200

    File reader now returns parsed HTML that can be queried

--- bblimport/src/import/filereader.service.ts
+++ bblimport/src/import/filereader.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
+import { parse, HTMLElement } from 'node-html-parser';
 import { join } from 'path';
 
 @Injectable()
 export class FileReaderService {
     // TODO Get directory of data files from environment variable, input argument, or something like that
-    readFile(filename: string, directory: string = '../bbldownload/bbl-site/tloeg.bbleague.se'): string {
+    readFile(filename: string, directory: string = '../bbldownload/bbl-site/tloeg.bbleague.se'): HTMLElement {
         const filepath = join(process.cwd(), directory, filename);
-        return readFileSync(filepath).toString();
+        return parse(readFileSync(filepath).toString());
     }
 }
--- bblimport/src/import/import.service.ts
+++ bblimport/src/import/import.service.ts
@@ -8,6 +8,6 @@ export class ImportService {
     importEverything(): void {
         console.log("Importing not implemented yet");
         // TODO Import everything by calling specific import functions in the correct order based on data dependencies
-        console.log(this.fileReaderService.readFile('index.html')); // TODO Remove this call, only temporarily here to make code execute
+        console.log(this.fileReaderService.readFile('index.html').querySelector('#updnote')); // TODO Remove this call, only temporarily here to make code execute
     }
 }
