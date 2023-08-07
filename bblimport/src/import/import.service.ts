65bd053f9b08af450ef19316282e75b6f78c4d13
Jens Rydholm <spacejens@gmail.com>
2023-08-07 22:43:40 +0200

    TODO When importing team types, consider that some are no longer available

--- bblimport/src/import/import.service.ts
+++ bblimport/src/import/import.service.ts
@@ -11,7 +11,7 @@ export class ImportService {
 
     importEverything(): void {
         this.coachesService.uploadCoaches(this.coachesService.getCoaches());
-        // TODO Import team types before importing teams
+        // TODO Import team types before importing teams (note that some team types are no longer in the list although they do have an ID, e.g. Simyin for Djungelvrålarna)
         this.teamsService.uploadTeams(this.teamsService.getTeams());
         console.log("Importing not implemented yet");
         // TODO Import everything by calling specific import functions in the correct order based on data dependencies
