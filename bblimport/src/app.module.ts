3cfee8fd4e681217f3daaaf1f5b32c8936110a01
Jens Rydholm <spacejens@gmail.com>
2023-08-07 19:01:51 +0200

    Removed example service/controller from bblimport

--- bblimport/src/app.module.ts
+++ bblimport/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ImportModule } from './import/import.module';
 import { SanityCheckModule } from './sanitycheck/sanitycheck.module';
 
 @Module({
   imports: [ImportModule, SanityCheckModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- bblimport/src/main.ts
+++ bblimport/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
 import { ImportService } from './import/import.service';
 import { SanityCheckService } from './sanitycheck/sanitycheck.service';
 
@@ -10,7 +9,5 @@ async function bootstrap() {
   importService.importEverything();
   const sanityCheckService = app.get(SanityCheckService);
   sanityCheckService.sanityCheckEverything();
-  const appService = app.get(AppService);
-  console.log(appService.getHello());
 }
 bootstrap();
