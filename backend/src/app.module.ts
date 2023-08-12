7660fbd95bd42abca382ccecce70d3af5dfd46ce
Jens Rydholm <spacejens@gmail.com>
2023-08-12 11:02:20 +0200

    Creating persistence module and Prisma service using nest generate commands

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
-  imports: [],
+  imports: [PersistenceModule],
   controllers: [AppController],
   providers: [AppService],
 })
