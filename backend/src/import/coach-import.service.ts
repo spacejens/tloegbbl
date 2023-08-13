bad4ff4a61d0c081c1d88badaf56be9e9bba1ef0
Jens Rydholm <spacejens@gmail.com>
2023-08-13 11:14:42 +0200

    TODO Sketch of how importing should work, in a bit more detail

--- backend/src/import/coach-import.service.ts
+++ backend/src/import/coach-import.service.ts
@@ -11,7 +11,10 @@ export class CoachImportService {
   importCoach(
     request: ImportRequestEnvelope<ImportCoach>,
   ): ImportResponseEnvelope<ImportCoach> {
-    // TODO Implement importing coaches
+    // TODO Find existing coach if it exists (using internal ID, external ID, name? Verify no conflicting internal/external IDs?)
+    // TODO If existing coach found, update it (directives would control if existing data points get overwritten or not)
+    // TODO If no existing coach, create new using the input
+    // TODO Return the resulting updated/new state of the coach
     return {
       status: ImportResponseStatus.Failed,
     };
