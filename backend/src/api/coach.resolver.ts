7ebc59d6a127c76ab3afc7ee0830363bec5e2abb
Jens Rydholm <spacejens@gmail.com>
2023-08-19 14:07:28 +0200

    Simplified API code by using DTO instead of defining an API-specific model

--- backend/src/api/coach.resolver.ts
+++ backend/src/api/coach.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { CoachService } from '../persistence/coach.service';
-import { Coach } from './coach.model';
+import { Coach } from '../dtos';
 
 @Resolver((of) => Coach)
 export class CoachResolver {
@@ -8,12 +8,6 @@ export class CoachResolver {
 
   @Query((returns) => Coach, { nullable: true })
   async coach(@Args('id', { type: () => Int }) id: number): Promise<Coach> {
-    const coach = await this.coachService.findCoachById(id);
-    return (
-      coach && {
-        id: coach.id,
-        name: coach.name,
-      }
-    );
+    return this.coachService.findCoachById(id);
   }
 }
--- backend/src/dtos.ts
+++ backend/src/dtos.ts
@@ -1,14 +1,20 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
 export class ExternalId {
   id?: number;
   externalId: string;
   externalSystem: string;
 }
 
+@ObjectType()
 export class CoachReference {
+  @Field((type) => Int)
   id?: number;
   externalIds?: ExternalId[];
 }
 
+@ObjectType()
 export class Coach extends CoachReference {
+  @Field()
   name: string;
 }
