9ae032f90c4a36d7aade3fe33874a8258c670e5d
Jens Rydholm <spacejens@gmail.com>
2023-08-19 22:19:24 +0200

    Unit tests for CoachService

--- backend/src/persistence/coach.service.spec.ts
+++ backend/src/persistence/coach.service.spec.ts
@@ -5,6 +5,7 @@ import { mock } from 'jest-mock-extended';
 
 describe('CoachService', () => {
   let service: CoachService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,9 +16,92 @@ describe('CoachService', () => {
     }).compile();
 
     service = module.get<CoachService>(CoachService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('countCoaches', () => {
+    it('should return the count', async () => {
+      prismaService.coach.count = jest.fn().mockReturnValue(23);
+      const result = await service.countCoaches();
+      expect(result).toBe(23);
+    });
+  });
+
+  describe('findCoachById', () => {
+    it('should return found coach without external IDs', async () => {
+      prismaService.coach.findUnique = jest.fn().mockReturnValue({
+        id: 23,
+        externalId: [],
+        name: 'Found',
+      });
+      const result = await service.findCoachById(23);
+      expect(result).toEqual({
+        id: 23,
+        externalIds: [],
+        name: 'Found',
+      });
+      expect(prismaService.coach.findUnique).toHaveBeenCalledWith({
+        where: {
+          id: 23,
+        },
+        include: {
+          externalId: true,
+        },
+      });
+    });
+
+    it('should return found coach with external IDs', async () => {
+      prismaService.coach.findUnique = jest.fn().mockReturnValue({
+        id: 23,
+        externalId: [
+          {
+            id: 45,
+            externalId: 'ExtId',
+            externalSystem: 'ExtSys',
+          },
+        ],
+        name: 'Found',
+      });
+      const result = await service.findCoachById(23);
+      expect(result).toEqual({
+        id: 23,
+        externalIds: [
+          {
+            id: 45,
+            externalId: 'ExtId',
+            externalSystem: 'ExtSys',
+          },
+        ],
+        name: 'Found',
+      });
+      expect(prismaService.coach.findUnique).toHaveBeenCalledWith({
+        where: {
+          id: 23,
+        },
+        include: {
+          externalId: true,
+        },
+      });
+    });
+
+    it('should return undefined if not found', async () => {
+      prismaService.coach.findUnique = jest.fn().mockReturnValue(undefined);
+      const result = await service.findCoachById(23);
+      expect(result).toEqual(undefined);
+      expect(prismaService.coach.findUnique).toHaveBeenCalledWith({
+        where: {
+          id: 23,
+        },
+        include: {
+          externalId: true,
+        },
+      });
+    });
+  });
+
+  // TODO Unit test the rest of the methods
 });
