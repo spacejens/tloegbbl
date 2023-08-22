86e8e07e4063a948d73cf5713dfade3733cc0a9a
Jens Rydholm <spacejens@gmail.com>
2023-08-22 15:55:48 +0200

    Import avoids uploading team types more than once

--- bblimport/src/import/team-types.service.ts
+++ bblimport/src/import/team-types.service.ts
@@ -10,8 +10,14 @@ export type BblTeamType = BblTeamTypeReference & {
 
 @Injectable()
 export class TeamTypesService {
+  private uploadedTeamTypes = Array<string>();
   async uploadTeamType(teamType: BblTeamType): Promise<void> {
-    // TODO Ensure no duplicate uploads
+    // Ensure no duplicate uploads
+    if (this.uploadedTeamTypes.indexOf(teamType.id) != -1) {
+      return;
+    }
+    this.uploadedTeamTypes.push(teamType.id);
+    // TODO Can the duplicate upload detection be more clean? Would be nice with an object equality check of the whole team type
     // TODO Upload team type to backend
     // TODO Console log the result from server
   }
