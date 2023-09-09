10b6589cb7a67112d31e1504bfba3e5f38c40526
Jens Rydholm <spacejens@gmail.com>
2023-09-09 13:13:08 +0200

    TODO Injuries of the same type give info to acting players' actions

--- bblimport/src/import/matches.service.ts
+++ bblimport/src/import/matches.service.ts
@@ -311,6 +311,7 @@ export class MatchesService {
     // TODO Consolidate into a smaller set, joining events that belong together
     // TODO In case of serious injuries, the acting row's exact consequence will be unknown, but consolidate anyway (when possible)
     // TODO If the same player caused every injury of a severity level, consolidate to all the consequence rows
+    // TODO If all serious injuries have the same consequence type (e.g. niggling), that information can be used to detail the acting players' actions
     // TODO Avoid consolidation for matches that are linked to other matches (multiplayer games)
     return matchEvents;
   }
