21c81c448f0f0a0fd9e3886310e6cb36b6d379eb
Jens Rydholm <spacejens@gmail.com>
2024-07-02 15:21:03 +0200

    Request logging to troubleshoot backend when importing

---
+++ backend/src/app-logger.middleware.ts
@@ -0,0 +1,29 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AppLoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    this.logger.log(
+      `Body: ${JSON.stringify(request.body)}`
+    );
+
+    /* TODO More reasonable logging
+    const { ip, method, path: url } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('close', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+
+      this.logger.log(
+        `${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`
+      );
+    });
+    */
+
+    next();
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersistenceModule } from './persistence/persistence.module';
 import { ImportModule } from './import/import.module';
 import { RestModule } from './rest/rest.module';
+import { AppLoggerMiddleware } from './app-logger.middleware';
 
 @Module({
   imports: [PersistenceModule, ImportModule, RestModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AppLoggerMiddleware).forRoutes('match-event'); // TODO Only apply logging where needed
+  }
+}
