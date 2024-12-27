b49a91fa91733f511820a28eac685d46d9f76bc2
Jens Rydholm <spacejens@gmail.com>
2024-12-27 22:49:47 +0100

    Add unit tests for HTTP POST

--- bblimport/src/api-client/api-client.service.spec.ts
+++ bblimport/src/api-client/api-client.service.spec.ts
@@ -6,6 +6,7 @@ import { ConfigService } from '@nestjs/config';
 
 describe('ApiClientService', () => {
   let service: ApiClientService;
+  let configService: ConfigService;
   let httpService: HttpWrapperService;
 
   beforeEach(async () => {
@@ -18,19 +19,60 @@ describe('ApiClientService', () => {
     }).compile();
 
     service = module.get<ApiClientService>(ApiClientService);
-    httpService = module.get<HttpWrapperService>(HttpWrapperService);
 
-    httpService.post = jest.fn().mockImplementation(() => {
-      return {
+    configService = module.get<ConfigService>(ConfigService);
+    configService.get = jest.fn().mockImplementation((propertyPath) => {
+      switch(propertyPath) {
+        case 'BACKEND_API_URL':
+          return 'http://localhost:3000/';
+        default:
+          throw new Error(`Unexpected propertyPath ${propertyPath}`);
+      }
+    });
+
+    httpService = module.get<HttpWrapperService>(HttpWrapperService);
+  });
+
+  describe('post', () => {
+    const postHeaders = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    it('expects HTTP 201 for successful POST', async () => {
+      const expectedResult = {
+        status: 201,
+        data: {},
+      };
+      httpService.post = jest.fn().mockReturnValue(expectedResult);
+      const result = await service.post('some/path', { argument: 'value' });
+      expect(httpService.post).toHaveBeenCalledWith(
+        'http://localhost:3000/some/path',
+        { argument: 'value' },
+        postHeaders,
+      );
+      expect(result).toStrictEqual(expectedResult);
+    });
+
+    it('fails when server provides unexpected status code', async () => {
+      const unexpectedStatusResult = {
         status: 200,
         data: {},
       };
+      httpService.post = jest.fn().mockReturnValue(unexpectedStatusResult);
+      await expect(service.post('some/path', {})).rejects.toThrow();
+    });
+
+    it('fails when server returns data.errors', async () => {
+      const dataErrorsResult = {
+        status: 201,
+        data: {
+          errors: [],
+        },
+      };
+      httpService.post = jest.fn().mockReturnValue(dataErrorsResult);
+      await expect(service.post('some/path', {})).rejects.toThrow();
     });
   });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  // TODO Add unit tests for HTTP POST
 });
